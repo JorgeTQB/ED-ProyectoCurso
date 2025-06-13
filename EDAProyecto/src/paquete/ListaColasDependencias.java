07cff7da597fcc473a911ba4e813f470cccfb0a0
JorgeTQB <j.quispe.b1304@gmail.com>
2025-06-13 21:11:17 +0000

    Agregar ListaColasDependencias

---
+++ EDAProyecto/src/paquete/ListaColasDependencias.java
@@ -0,0 +1,20 @@
+
+
+public class ListaColasDependencias{
+
+    private Dependencia inicio;
+    private Dependencia bienestar;
+    //etc...
+
+    private DoublyLinkedList listaColas;
+
+    public ListaColasDependencias(){
+
+        listaColas.agregar(inicio);
+        listaColas.agregar(bienestar);
+        
+    }
+
+
+
+}
